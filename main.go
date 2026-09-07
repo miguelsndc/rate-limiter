fb33bc11ca697bccba949d636fe5cf8155f69bc7
miguel <mglsndc@gmail.com>
2026-09-07 19:53:44 -0300

    test rate limiter for correctness and consistency

--- main.go
+++ main.go
@@ -2,30 +2,36 @@ package main
 
 import (
 	"fmt"
-	"rate-limiter/policies"
-	"time"
 	"math/rand/v2"
+	"rate-limiter/policies"
+	"sync"
+	"time"
 )
 
 func getKey() string {
-	keys := []string {"miguel", "samuel", "gustavo", "dyers"}
+	keys := []string{"miguel", "samuel", "gustavo", "dyers", "wesley"}
 	index := rand.IntN(len(keys))
 	return keys[index]
 }
 
-func main() {
-	lim := policies.NewLimiter(5, 500 * time.Millisecond)
-    for {
-		key := getKey()
-        noOfRequests := rand.IntN(20)
-		for range noOfRequests {
-			if lim.Allow(key) {
-				fmt.Println("key ", key, " allowed at ", time.Now())
-			} else {
-				fmt.Println("key ", key, " denied at ", time.Now())
-			}
-			time.Sleep(time.Duration(rand.IntN(200)) * time.Millisecond)
+func worker(id int, key string, lim *policies.Limiter) {
+	for range 10 {
+		if lim.Allow(key) {
+			fmt.Printf("%d: worker with key %s allowed at %v\n", id, key, time.Now())
+		} else {
+			fmt.Printf("%d: worker with key %s denied at %v\n", id, key, time.Now())
 		}
-		time.Sleep(time.Duration(rand.IntN(200)) * time.Millisecond)
-	}    
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func main() {
+	lim := policies.NewLimiter(5, 400*time.Millisecond)
+	var wg sync.WaitGroup
+	for i := range 100 {
+		wg.Go(func() {
+			worker(i, getKey(), lim)
+		})
+	}
+	wg.Wait()
 }
